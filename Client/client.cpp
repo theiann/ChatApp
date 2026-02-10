626c2e015a42713af78d3a70321a3a56e14d6849
theiann <2015iann@gmail.com>
2026-02-10 12:40:00 -0600

    added server name to resolved ip address translation

--- Client/client.cpp
+++ Client/client.cpp
@@ -4,7 +4,8 @@
 #define SERVER_PORT 9999
 #define MAX_LINE 256
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     std::cout << "argc = " << argc << std::endl;
     if (argc < 2)
     {
@@ -22,4 +23,24 @@ int main(int argc, char **argv){
     }
     std::cout << "Winsock initialized.";
     return 0;
+
+    // translate the server name or IP address (128.90.54.1) to resolved IP address
+    unsigned int ipaddr;
+    // If the user input is an alpha name for the host, use gethostbyname()
+    // If not, get host by addr (assume IPv4)
+    if (isalpha(argv[1][0]))
+    { // host address is a name
+        hostent *remoteHost = gethostbyname(argv[1]);
+        if (remoteHost == NULL)
+        {
+            std::cout << "Host not found";
+            WSACleanup();
+            return 1;
+        }
+        ipaddr = *((unsigned long *)remoteHost->h_addr);
+    }
+    else //"128.90.54.1"
+
+    
+        ipaddr = inet_addr(argv[1]);
 }
